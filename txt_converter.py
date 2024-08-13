d547c46d76c17f9bdf64bb82e432f6996387f1a1
Tascota <2166908531@qq.com>
2024-08-14 00:13:29 +0800

    v1.2 - sundries added

--- txt_converter.py
+++ txt_converter.py
@@ -5,15 +5,32 @@ origin = open(file_name, "r", encoding="utf-8")
 def convert(org, name):
     name_new = "new_" + name
     file = open(name_new, "w", encoding="utf-8")
+    li_count = 0
 
     lines = org.readlines()
     for line in lines:
         if line == "\n":
-            line_new = "<br>\n"
+            if li_count != 0:
+                line_new = "</ul>\n<br>\n"
+            else:
+                line_new = "<br>\n"
+            li_count = 0
+        elif "	⁃	" in line:
+            line_new = line.replace("	⁃	", "")
+            line_new = line_new.replace("\n", "")
+            if li_count == 0:
+                line_new = "<ul>\n" + "<li>" + line_new + "</li>" + "\n"
+            else:
+                line_new = "<li>" + line_new + "</li>" + "\n"
+            li_count += 1
         else:
             line_new = line.replace("\n", "")
-            line_new = "<p>" + line_new + "</p>" + "\n"
-        print(line_new)
+            if li_count != 0:
+                line_new = "</ul>\n" + "<p>" + line_new + "</p>" + "\n"
+            else:
+                line_new = "<p>" + line_new + "</p>" + "\n"
+            li_count = 0
+        print(line_new, li_count)
         file.write(line_new)
 
 
