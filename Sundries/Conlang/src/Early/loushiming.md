59d1d9f2e33c3be1beab4c2d643cdddbd883c72b
Tascota <femtocota@gmail.com>
2024-12-01 17:18:27 +0800

    v1.5.5

--- Sundries/Conlang/src/Early/loushiming.md
+++
@@ -1,6 +0,0 @@
-# 陋室铭 / Dabuskumty Žerei'u
-<h2>Lyou Yusci</h2>
-<br>
-<p>Nonsd unizaud ubeivud, sarisaud taulusd et sanarëd. Glahuasd unizaud eltumud, sarisaud longd et kusvarëd. Twi brusau dabuskum, kot u skia brusau betzirat'ty. Rakomitd nube'a'aud steëd et kurinova'aud, Remd žira'aud këtënd et kuka'aud. Saitau utë sautalusd, ato'au u'utë ulusd. Bankusau dusotau isi gutscind, ikaherau budžerei'ud. Usarisau pokstia sanad dabusarëd kerebënkum, et betalizud tairahrëd korpodrond. Nanyangty Zčugekum, et Scishuty Đriyunting. Kongđri saitlu: “Woi'ud brusauvod dabus?”</p>
-<br>
-<p>时间：2022-03-05</p>
--- txt_converter.py
+++ txt_converter.py
@@ -1,8 +1,9 @@
-file_name = input()
+file_name = input("File name:")
 origin = open(file_name, "r", encoding="utf-8")
+mode = input("Choose a mode [n(nbr)/c(c3)]:")
 
 
-def convert(org, name):
+def convert(org, name, mode):
     name_new = "new_" + name
     file = open(name_new, "w", encoding="utf-8")
     li_count = 0
@@ -10,7 +11,9 @@ def convert(org, name):
     lines = org.readlines()
     for line in lines:
         if line == "\n":
-            if li_count != 0:
+            if mode == "nbr":
+                line_new = ""
+            elif li_count != 0:
                 line_new = "</ul>\n<br>\n"
             else:
                 line_new = "<br>\n"
@@ -34,16 +37,53 @@ def convert(org, name):
         file.write(line_new)
 
 
-while True:
-    convert(origin, file_name)
+def convertConlang(org, name, mkde):
+    name_new = "new_" + name
+    file = open(name_new, "w", encoding="utf-8")
+    count = 0
 
+    lines = org.readlines()
+    for line in lines:
+        if line == "\n":
+            line_new = ""
+            print(line_new, count)
+            file.write(line_new)
+            continue
+        
+        line_new = line.replace("\n", "")
+
+        if lines.index(line) == 0 or count == 0:
+            line_new = "<div class=\"paragraph\">\n    <p class=\"original\">" + line_new + "</p>\n"
+            count = 1
+        elif count == 1:
+            if mode == "c":
+                line_new = "    <p class=\"translation\">" + line_new + "</p>\n</div>\n\n"
+                count = 0
+            else:
+                line_new = "    <p class=\"translation\">" + line_new + "</p>\n"  
+                count += 1
+        elif count == 2:
+            line_new = "    <p class=\"ref-translation\">" + line_new + "</p>\n</div>\n\n"
+            count = 0
+        
+        print(line_new, count)
+        file.write(line_new)
+
+while True:
+    if mode == ("c" or "c3"):
+        convertConlang(origin, file_name, mode)
+    else:
+        convert(origin, file_name, mode)
+
+    mode = ""
     file_name = ""
     origin = ""
 
     whether_continue = input("Continue(y)?")
     if whether_continue == "y":
-        file_name = input()
+        file_name = input("File name:")
         origin = open(file_name, "r", encoding="utf-8")
+        mode = input("Choose a mode [n(nbr)/c(c3)]:")
         continue
     else:
         break
