6ae38b952589517a25161ace01cbd43429591401
Tascota <femtocota@gmail.com>
2024-12-09 22:37:29 +0800

    v1.5.9

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function generateRandomNumber(list) {
 }
 
 /* random quote */
-const madoka = ["奇迹和魔法都是存在的", "「奇跡も、魔法も、あるんだよ」", "鹿目圆", "《魔法少女小圆》", "ja-jp"];
+const madoka = ["奇迹和魔法都是存在的", "「奇跡も、魔法も、あるんだよ」", "美树沙耶香", "《魔法少女小圆》", "ja-jp"];
 const shuumatsu = ["没到最后就不会结束哦", "「終わるまでは終わらないよ」", "千户、尤莉", "《More One Night》", "ja-jp"];
 const hotaru = ["啊啊 苍蓝之色 黎明与萤火", "「ああ 藍の色 夜明けと蛍」", "n-buna", "《黎明与萤火》", "ja-jp"];
 const kininaru = ["我很好奇！", "「私、気になります！」", "千反田爱瑠", "《冰菓》", "ja-jp"];
