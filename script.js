24d69f5c5977b91405a0c2970db590622b1e55b1
Tascota <femtocota@gmail.com>
2025-07-15 15:04:39 +0800

    v1.8

--- script.js
+++ script.js
@@ -2,21 +2,27 @@ function generateRandomNumber(list) {
     const randomSelector = Math.floor (Math.random() * list.length);
     return randomSelector;
 }
+let quote_now;
+let showingOriginal = true;
 
 /* random quote */
-const madoka = ["奇迹和魔法都是存在的", "「奇跡も、魔法も、あるんだよ」", "美树沙耶香", "《魔法少女小圆》", "ja-jp"]; // anime
-const shuumatsu = ["没到最后就不会结束哦", "「終わるまでは終わらないよ」", "千户、尤莉", "《More One Night》", "ja-jp"];
-const kininaru = ["我很好奇！", "「私、気になります！」", "千反田爱瑠", "《冰菓》", "ja-jp"];
-const nyanpasu = ["喵帕斯", "「にゃんぱすー」", "宫内莲华", "《悠哉日常大王》", "ja-jp"]
-const keion = ["不是很精彩呢", "「あんまり上手くないですね」", "平泽唯、中野梓", "《K-ON!》", "ja-jp"]
-const tomori = ["因为我的歌就是内心的呐喊", "「だって、私の歌は、心の叫びだから」", "高松灯", "《BanG Dream! It's MyGO!!!!!》", "ja-jp"]
-const kimoi = ["你真恶心", "「気持ち悪い」", "惣流·明日香·兰格雷", "《新世纪福音战士剧场版 Air/真心为你》", "ja-jp"]
-const aoitori = ["我喜欢霙的双簧管", "「私、みぞれのオーボエが好き」", "伞木希美", "《利兹与青鸟》", "ja-jp"]
-const euphonium = ["于是，下一首曲子即将奏响！", "「そして、次の曲が始まるのです！」", "黄前久美子", "《吹响！上低音号》", "ja-jp"];
-
-const hotaru = ["啊啊 苍蓝之色 黎明与萤火", "「ああ 藍の色 夜明けと蛍」", "n-buna", "《黎明与萤火》", "ja-jp"]; // song
-const kagerou = ["这个夏天的故事，永远不会结束", "「この夏の物語は、終わらない」", "じん", "阳炎 Project", "ja-jp"];
-const henceforth = ["啊啊 夏日如今再度", "「あぁ 夏は今もう一回」", "Orangestar", "《Henceforth》", "ja-jp"];
+// anime
+const madoka = ["奇迹和魔法都是存在的", "「奇跡も、魔法も、あるんだよ」", "美树沙耶香", "美樹さやか", "《魔法少女小圆》", "『魔法少女まどか☆マギカ』", "ja-JP", "1"];
+const shuumatsu = ["没到最后就不会结束哦", "「終わるまでは終わらないよ」", "千户、尤莉", "チト、ユーリ", "《More One Night》", "「More One Night』", "ja-JP", "1"];
+const kininaru = ["我很好奇！", "「私、気になります！」", "千反田爱瑠", "千反田える", "《冰菓》", "『氷菓』", "ja-JP", "1"];
+const nyanpasu = ["喵帕斯", "「にゃんぱすー」", "宫内莲华", "宮内れんげ", "《悠哉日常大王》", "『のんのんびより』", "ja-JP", "1"]
+const keion = ["不是很精彩呢", "「あんまり上手くないですね」", "平泽唯、中野梓", "平沢唯、中野梓", "《轻音少女》", "『けいおん！』", "ja-JP", "1"]
+const tomori = ["因为我的歌就是内心的呐喊", "「だって、私の歌は、心の叫びだから」", "高松灯", "高松燈", "《BanG Dream! It's MyGO!!!!!》", "『BanG Dream! It’s MyGO!!!!!』", "ja-JP", "1"]
+const kimoi = ["你真恶心", "「気持ち悪い」", "惣流·明日香·兰格雷", "惣流・アスカ・ラングレー", "《新世纪福音战士剧场版 Air/真心为你》", "『新世紀エヴァンゲリオン劇場版 Air/まごころを、君に』", "ja-JP", "1"]
+const aoitori = ["我喜欢霙的双簧管", "「私、みぞれのオーボエが好き」", "伞木希美", "傘木希美", "《利兹与青鸟》", "『リズと青い鳥』", "ja-JP", "1"]
+const euphonium = ["于是，下一首曲子即将奏响！", "「そして、次の曲が始まるのです！」", "黄前久美子", "黄前久美子", "《吹响吧！上低音号》", "『響け！ユーフォニアム』", "ja-JP", "1"];
+const mono = ["就算变成大人，类似作业的东西也还是不会消失嘛", "「大人になっても、宿題っぽいのはなくなんないですね」", "雾山杏", "霧山アン", "《mono 女孩》", "『mono』", "ja-JP", "1"]
+const yojouhan = ["你必须接受现在的你，明白你不会成为除此以外的任何人", "「今、ここにいる君以外、ほかの何者にもなれない自分を認めなくてはいけない」", "樋口清太郎", "樋口清太郎", "《四叠半神话大系》", "『四畳半神話大系』", "ja-JP", "1"]
+// song
+const hotaru = ["啊啊 苍蓝之色 黎明与萤火", "「ああ 藍の色 夜明けと蛍」", "n-buna", "n-buna", "《黎明与萤火》", "「夜明けと蛍」", "ja-JP", "2"];
+const kagerou = ["这个夏天的故事，永远不会结束", "「この夏の物語は、終わらない」", "じん", "じん", "阳炎 Project", "陽炎 Project", "ja-JP", "2"];
+const henceforth = ["啊啊 夏日如今再度", "「あぁ 夏は今もう一回」", "Orangestar", "Orangestar", "《Henceforth》", "「Henceforth」", "ja-JP", "2"];
+const diamond = ["拿音乐去赚钱 这样的我们已经没救了吧", "「音楽で金を稼いでいる俺たちは終わりだ」", "神绮一织", "カンザキイオリ", "《钻石》", "「ダイアモンド」", "ja-JP", "2"]
 
 const quoteList = [
     madoka,
@@ -28,29 +34,81 @@ const quoteList = [
     kimoi,
     aoitori,
     euphonium,
+    mono,
+    yojouhan,
     hotaru,
     kagerou,
-    henceforth
+    henceforth,
+    diamond
 ];
 
-function generateRandomQuote() {
-    const quoteSelected = quoteList[generateRandomNumber(quoteList)];
-    const quote = quoteSelected[0];
-    const originalQuote = quoteSelected[1];
-    const author = quoteSelected[2];
-    const origin = quoteSelected[3];
-    const language = quoteSelected[4];
+function writeQuote() {
+    document.getElementById("quote").textContent = quote_now[1];
+    document.getElementById("quote_author").textContent = quote_now[3];
+    const lang = quote_now[6];
+    const mode = quote_now[7];
 
-    document.getElementById("quote").textContent = "“" + quote + "”";
-    document.getElementById("original_quote").textContent = originalQuote;
-    document.getElementById("quote_author").textContent = author;
-    document.getElementById("quote_origin").textContent = origin;
-    document.getElementById("original_quote").setAttribute("lang", language);
+    if (lang == "ja-JP") {
+        if (mode == "1") {
+            document.getElementById("quote_origin").textContent = "（" + quote_now[5] + "）";
+        }
+        if (mode == "2") {
+            document.getElementById("quote_origin").textContent = "（" + quote_now[5] + "より）";
+        }
+    }
+
+    document.getElementById("quote").setAttribute("lang", lang);
+    document.getElementById("quote_author").setAttribute("lang", lang);
+    document.getElementById("quote_origin").setAttribute("lang", lang);
+
+    console.log("lang:", lang, "mode:", mode, "quote_now:", quote_now);
+}
+
+function generateRandomQuote() {
+    let quoteSelected = quoteList[generateRandomNumber(quoteList)];
+    quote_now = quoteSelected;
+    writeQuote();
+    return quoteSelected;
+}
+
+function translateQuote() {
+    if (!quote_now) 
+        return;
+    const mode = quote_now[7];
+
+    document.getElementById("quote").textContent = "“" + quote_now[0] + "”";
+    document.getElementById("quote_author").textContent = quote_now[2];
+
+    if (mode == "1") {
+        document.getElementById("quote_origin").textContent = "，" + quote_now[4];
+    }
+    if (mode == "2") {
+        document.getElementById("quote_origin").textContent = "，" + quote_now[4] + "中";
+    }
+
+    document.getElementById("quote").setAttribute("lang", "zh-CN");
+    document.getElementById("quote_author").setAttribute("lang", "zh-CN");
+    document.getElementById("quote_origin").setAttribute("lang", "zh-CN");
+
+    console.log("mode:", mode, "quote_now:", quote_now);
 }
 
 generateRandomQuote();
+
 document.getElementById("quote_resetter").addEventListener("click", function() {
     generateRandomQuote();
+    showingOriginal = true;
+});
+
+document.getElementById("quote_translator").addEventListener("click", function() {
+    if (showingOriginal) {
+        translateQuote();
+    }
+    else {
+        writeQuote();
+    }
+
+    showingOriginal = !showingOriginal;
 });
 
 /* random song */
@@ -62,9 +120,10 @@ const vocaloidList = [
     "もうじき夏が終わるから - n-buna",
     "着火、カウントダウン - n-buna",
     "ずっと空を見ていた - n-buna",
-    "カエルのはなし- n-buna",
+    "カエルのはなし - n-buna",
     "アスノヨゾラ哨戒班 - Orangestar",
     "DAYBREAK FRONTLINE - Orangestar",
+    "Alice in 冷凍庫 - Orangestar",
     "Henceforth - Orangestar",
     "サマータイムレコード - じん",
     "群青レイン - じん",
@@ -81,12 +140,16 @@ const vocaloidList = [
     "アサヤケ - 一二三",
     "フィクションブルー - Ayase",
     "ヴァイオレッタ - Ayase",
+    "帝国少女 - R Sound Design",
     "ラグタレイン - 稲葉曇",
     "ハレハレヤ - 羽生まゐご",
     "阿吽のビーツ - 羽生まゐご",
     "ケガレの唄 - 羽生まゐご",
     "サイエンス - MIMI",
-    "眠り姫 - Guiano",
+    "フォニイ - ツミキ",
+    "花となれ - 雄之助",
+    "シャルル - バルーン",
+    "雨とペトラ - バルーン",
     "サンセットバスストップ - トーマ",
     "ツギハギスタッカート - とあ",
     "炉心融解 - iroha(sasaki)",
