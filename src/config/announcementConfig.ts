b7dd8238187d6d97da2959d76b5d35ab4917a6ac
Tascota <femtocota@gmail.com>
2026-09-21 22:44:55 +0800

    Article added

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,8 +2,8 @@ import type { AnnouncementConfig } from "../types/config";
 
 // 公告栏配置
 export const announcementConfig: AnnouncementConfig = {
-  title: "v2.2.1 - 2026-09-19", // 公告标题，填空使用i18n字符串Key.announcement
-  content: "添加了一张新画作。", // 公告内容
+  title: "v2.2.2 - 2026-09-21", // 公告标题，填空使用i18n字符串Key.announcement
+  content: "添加了一篇新文章及其相应分类。", // 公告内容
   closable: false, // 允许用户关闭公告
   link: {
     enable: true, // 启用链接
